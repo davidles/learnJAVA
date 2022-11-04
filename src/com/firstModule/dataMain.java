9902c1319ba2d8500b5ef934761fb5ea84d6e006
david lescano <lescanodavid07@gmail.com>
2022-11-04 15:11:35 -0300

    refactor package

--- src/com/firstModule/dataMain.java
+++ src/com/firstModule/dataMain.java
@@ -1,18 +1,7 @@
-/*
-Para este primer ejercicio tendréis que realizar lo siguiente:
+package com.firstModule;
 
-Crea un proyecto de Java desde 0
+public class dataMain {
 
-Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
-
-Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
-
-Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
- */
-
-package datos;
-
-public class Main {
     public static void main(String[] args) {
 
         int entero = 30;
