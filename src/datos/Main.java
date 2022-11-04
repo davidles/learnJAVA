7e53805f70df9a37b0f220f94216e0d9232d779f
david lescano <lescanodavid07@gmail.com>
2022-11-04 15:05:14 -0300

    Add boolean & float

--- src/datos/Main.java
+++ src/datos/Main.java
@@ -17,16 +17,20 @@ public class Main {
 
         int entero = 30;
         long enteroLogn = 100;
-        double doubleNum = 31.21;
-        boolean booleno = true;
+        double doubleNum = 31.21d;
+        float floatNum = 25.3f;
+        boolean boolenTrue = true;
+        boolean boolenFalse = false;
         String text = "Primera entrega";
 
         System.out.println("***************** Datos primitivos *****************");
         System.out.println(entero);
         System.out.println(enteroLogn);
         System.out.println(doubleNum);
-        System.out.println(booleno);
+        System.out.println(boolenTrue);
         System.out.println(text);
+        System.out.println(floatNum);
+        System.out.println(boolenFalse);
 
     }
 }
